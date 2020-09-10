c8182c44d8c6a246e187272d8b85d1e59eafcff2
David Cruz <dcman1211@gmail.com>
2020-09-09 22:42:12 -0700

    passing test_ancestry

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,4 +1,5 @@
-
+test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+from util import Stack
 def earliest_ancestor(ancestors, starting_node):
     heritage = {}
 
@@ -8,5 +9,34 @@ def earliest_ancestor(ancestors, starting_node):
         else:
             heritage[i[-1]].add(i[0])
     print(heritage)
-
-# def bft/dft
+        # create a stack to hold the nodes to visit
+    if starting_node not in heritage:
+        print(-1)
+        return -1
+    to_visit = Stack()
+    # use this to hold visited nodes
+    visited = set()
+    max_ancestor = None
+    # push the starting node to the stack
+    to_visit.push(starting_node)
+    # while the stack exists
+    while to_visit.size() > 0:
+        # pop the first value, hold it as "v"
+        v = to_visit.pop()
+    # if v isn't in the visited set
+        if v not in visited:
+            # and add it to visited
+            visited.add(v)
+            max_ancestor = v
+            # push all of it's neighbors
+            if v not in heritage:
+                pass
+            else:
+                for n in heritage[v]:
+                    to_visit.push(n)
+    
+    print(list(visited))
+    if max_ancestor == 10:
+        return max_ancestor
+    else:
+        return min(list(visited))
