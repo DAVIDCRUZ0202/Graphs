8d1089d3b0395919f273f280267be13f26c2ed0a
David Cruz <dcman1211@gmail.com>
2020-09-11 15:56:12 -0400

    997 moves passing

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,7 +1,7 @@
 from room import Room
 from player import Player
 from world import World
-from util import Stack
+from util import Queue
 import random
 from ast import literal_eval
 
@@ -11,10 +11,10 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-map_file = "maps/test_cross.txt"
+# map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -29,25 +29,48 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_graph = {}
 traversal_path = []
-def traverse(player, traversal_graph, traversal_path):
-    s = Stack()
-    visited = set()
-    s.push(player.current_room.id)
-    while s.size() > 0:
-        move = s.pop()
-        if move not in visited:
-            visited.add(move)
-            traversal_graph[move] = player.current_room.get_exits()
-            for path in traversal_graph[move]:
-                player.travel(path)
-                traversal_path.append(path)
-                s.push(player.current_room.id)
-                traversal_graph[player.current_room.id] = player.current_room.get_exits()
-            
-    return visited
+visited = set()
 
-traverse(player, traversal_graph, traversal_path)
-breakpoint()
+def traverse(player):
+    visited.add(player.current_room)
+    while len(visited) < len(room_graph):
+        x = bfs(player.current_room)
+        for room , direction in x:
+            if direction == None:
+                continue
+            traversal_path.append(direction)
+            player.travel(direction)
+            visited.add(room)
+
+
+def bfs(starting_room):
+    to_visit = Queue()
+
+    to_visit.enqueue([(starting_room,None)])
+
+    attempted_rooms = set()
+
+    while to_visit.size() > 0:
+
+        path = to_visit.dequeue()
+
+        room = path[-1][0]
+
+        if room not in attempted_rooms:
+
+            if room not in visited:
+
+                return path
+            
+            attempted_rooms.add(room)
+
+            for exit in room.get_exits():
+                connected_room = room.get_room_in_direction(exit)
+                to_visit.enqueue(path+[(connected_room,exit)])
+
+            
+traverse(player)
+
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
